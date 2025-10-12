bba0faeab1d9e2a69b92a66f8665511497d0c873
Siddhantkadam28 <113542239+Siddhantkadam28@users.noreply.github.com>
2025-10-12 15:37:51 +0530

    Update cleanup.sh

--- Script/cleanup.sh
+++ Script/cleanup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#set -e  # Uncomment this line to stop the script if any command fails
 
-# Define the target directory (optional)
-# TARGET_DIR="/home/ec2-user/my-python-app"
 
 echo "Removing old files..."
-# rm -rf "$TARGET_DIR"/*   # Use this if TARGET_DIR is defined
+
 rm -rf "/home/ec2-user/my-python-app"/*
 echo "Old files removed successfully."
