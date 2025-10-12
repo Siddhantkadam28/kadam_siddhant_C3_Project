52fc242a053ebec5dba9dad2606bdb24bfd63572
Siddhantkadam28 <113542239+Siddhantkadam28@users.noreply.github.com>
2025-10-12 16:28:58 +0530

    Update cleanup.sh

--- Script/cleanup.sh
+++ Script/cleanup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-echo "Removing old files..."
-
-rm -rf "/home/ec2-user/my-python-app"/*
-echo "Old files removed successfully."
+echo "Cleaning up old deployment files..."
+sudo rm -rf /home/ec2-user/my-python-app/*
+echo "Cleanup complete."
