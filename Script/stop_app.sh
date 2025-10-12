ee1073fd4f67b283fff1da6afc3b72c6538e851a
Siddhantkadam28 <113542239+Siddhantkadam28@users.noreply.github.com>
2025-10-12 16:31:56 +0530

    Update stop_app.sh

--- Script/stop_app.sh
+++ Script/stop_app.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# Find the process ID(s) of the running 'sid.py' Python script
 PIDS=$(pgrep -f "python3 sid.py")
+
 if [ -n "$PIDS" ]; then
+    # If a process is found, terminate it forcefully
     kill -9 $PIDS
-    echo "Old process killed"
+    echo "Previous instance of sid.py terminated successfully."
 else
-    echo "No old process running"
+    # If no process is running, print a message
+    echo "No running instance of sid.py found."
 fi
